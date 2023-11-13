c9528aeafb95013ba827250a4fda08a75dfed845
Hamza Karoui <hamza.karoui552@gmail.com>
2023-11-12 19:09:44 -0500

    Update script.py

--- script.py
+++ script.py
@@ -10,6 +10,6 @@ script_name = sys.argv[0]
 badhash = sys.argv[1]
 goodhash = sys.argv[2]
 
-os.system(f'git bisect start {badhash} {goodhash}') 
+os.system(f'git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c') 
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
